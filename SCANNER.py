5f5aad61db0734d1854f4423b12019f2e61d6b85
ejkutz <79886893+ejkutz@users.noreply.github.com>
2023-04-11 01:02:45 -0500

    Update SCANNER.py

--- SCANNER.py
+++ SCANNER.py
@@ -2,10 +2,12 @@ import sys
 import dpkt
 import socket
 from dpkt.compat import compat_ord
+from decimal import Decimal
 
 f = open(sys.argv[1], 'rb')
 pcap = dpkt.pcap.Reader(f)
-
+#times
+times = []
 #Logfiles to output for each attack
 #arpspoof list of tuples formatted (src mac, dst mac, packet number)
 arpspoof_log = []
@@ -53,6 +55,13 @@ def is_udp(protocol_data: dpkt.udp.UDP) -> bool:
         return True
     return False
 
+def is_tcp_handshake(tcp_data: dpkt.tcp.TCP) -> bool:
+    if(type(tcp_data) != dpkt.tcp.TCP):
+        return False
+    if (tcp_data.flags == dpkt.tcp.TH_CWR | dpkt.tcp.TH_ECE):
+        return True
+    return False
+
 packet_no = 0
 dst_packets= {
   #"192.168.0.100": {"port_nums": [], "packet_nums": []}, #{ip: {"port_nums": [ports], "packet_nums": [packets]}}
@@ -60,6 +69,7 @@ dst_packets= {
   #"192.168.0.1":   {"port_nums": [], "packet_nums": []},
   #"192.168.0.101": {"port_nums": [], "packet_nums": []}
 } 
+times = {} # {ip: {port: {"packet_nums":[], "times":[]}}}
 for ts, buf in pcap:
     #Get packet data
     eth = dpkt.ethernet.Ethernet(buf)
@@ -94,7 +104,15 @@ for ts, buf in pcap:
     #        dst_packets[1]
     #    elif dst_ip ="192.168.0.1"
     #        dst_packets[2]
-    #TODO: synflood detection
+    #TODO: 
+    if(is_syn(protocol)):
+        if(not is_tcp_handshake(protocol)):
+            if dst_ip not in times:
+                times[dst_ip] = {}
+            if(dstport not in times[dst_ip]):
+                times[dst_ip][dstport] = {"packet_nums": [], "times": []}
+            times[dst_ip][dstport]["packet_nums"].append(packet_no)
+            times[dst_ip][dstport]["times"].append(ts)
     packet_no += 1
 
             
@@ -114,7 +132,9 @@ def output_port_scan(values: dict, ip: str) -> None:
       if idx > 0:
         print(",", end=" ")
       print(packet, end="")
-    print()
+      if idx > 100:
+        print()
+        break
 
 # detecting number of different ports
 for ip,packets in dst_packets.items():
@@ -124,15 +144,66 @@ for ip,packets in dst_packets.items():
         output_port_scan(packets, ip)
         continue
 
-for syn in synflood_log:
-    print("SYN floods")
-    print("Dst IP: %s" % syn[0])
-    print("Dst Port: %d" % syn[1])
-    print("Packet number:", end=" ")
-    for index, packet in enumerate(syn[2]):
-        if index > 0:
-            print(",", end=" ")
-        print(packet, end="")
+
+
+
+
+
+
+
+def output_synflood(ip: str, port: int, packets: list) -> None:
+    print("SYN floods!")
+    print("Dst IP: {0}".format(ip))
+    print("Dst Port: ", port)
+    print("Packet number: ", end="")
+    for idx in range(0, len(packets)):
+      if idx > 0:
+        print(",", end=" ")
+      print(packets[idx], end="")
     print()
 
+end_idx = 100
+for ip,ports in times.items():
+    for port, dicts in ports.items():
+        detected = 0
+        m_times = times[ip][port]#changes
+        detected_packets=[]
+        if(len(m_times['times']) > 100):
+            for idx,time in enumerate(m_times['times'][:-100]):
+                #use the Decimal typing as it doesn't have floating point precision math errors
+                #base python will not do accurate math with these doubles/floats
+                start_time = Decimal(time)
+
+                # get the end time, same reason as before
+                end_time = Decimal(m_times['times'][end_idx])
+            
+                # FPP subtraction
+                total_time = end_time - start_time
+                
+                #update the ending index)
+                end_idx += 1
+            
+                # if the total time between the 100 packets was less than 1 second, it was a flood
+                if(total_time < 1):
+                    # syn flood!
+                    detected += 1
+                    detected_packets.append(m_times['packet_nums'][idx])
+                if(detected >= 101):
+                    output_synflood(ip,port, detected_packets)#changes
+                    break
+            
+                
+
+
+#for syn in synflood_log:
+#    print("SYN floods")
+#    print("Dst IP: %s" % syn[0])
+#    print("Dst Port: %d" % syn[1])
+#    print("Packet number:", end=" ")
+#    for index, packet in enumerate(syn[2]):
+#        if index > 0:
+#            print(",", end=" ")
+#        print(packet, end="")
+#    print()
+#
 f.close()
